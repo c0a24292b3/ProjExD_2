ced95c99db2e3aa8c00890d5531f2a151e1bf0d4
waki <c0a24292b3@edu.teu.ac.jp>
2025-06-17 16:54:05 +0900

    演習1 ゲームオーバー画面

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import random
 import sys
 import pygame as pg
+import time
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -26,8 +27,46 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     if rct.top < 0 or HEIGHT < rct.bottom: # 縦方向の画面外判定
         tate = False
     return yoko, tate  # 横方向，縦方向の画面内判定結果を返す
-       
 
+def gameover(screen: pg.Surface) -> None:
+    """
+    ゲームオーバー時に，半透明の黒い画面上に「Game Over」と表示し，泣いているこうかとん画像を貼り付ける関数
+    """
+    # 画面を半透明の黒で覆う
+    blackscreen = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(blackscreen, (0, 0, 0), (0, 0, WIDTH, HEIGHT))#画面を覆う黒い四角を作成
+    blackscreen.set_alpha(200)  # 半透明にする
+    screen.blit(blackscreen, (0, 0))#貼り付ける。
+
+     # Game Over テキストの描画
+    font = pg.font.SysFont(None, 80)
+    text = font.render("Game Over", True, (255, 255, 255))#白文字でgameover
+    text_rct = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+    screen.blit(text, text_rct)#中央にgameoverをはりつけ
+
+    # 泣いているこうかとん画像の表示
+    crying_kk = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)#泣いているこうかとん
+    left_rct = crying_kk.get_rect(center=(text_rct.left - 60, HEIGHT // 2))#ゲームオーバーの左側にこうかとんを配置
+    screen.blit(crying_kk, left_rct)
+    right_rct = crying_kk.get_rect(center=(text_rct.right + 60, HEIGHT // 2))#ゲームオーバーの右側にこうかとんを配置
+    screen.blit(crying_kk, right_rct)
+    pg.display.update()  # 画面更新
+    time.sleep(5)  # 5秒待つ
+    
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    サイズの異なる爆弾Surfaceを要素としたリストと加速度リストを返す
+    """
+    bb_imgs = []
+    bb_accs = [i for i in range(1, 11)]  # 加速度リスト（1〜10）
+
+    for r in range(1, 11):
+        bb_img = pg.Surface((20 * r, 20 * r))
+        bb_img.set_colorkey((0, 0, 0))
+        pg.draw.circle(bb_img, (255, 0, 0), (10 * r, 10 * r), 10 * r)
+        bb_imgs.append(bb_img)  # リストに追加
+
+    return bb_imgs, bb_accs
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -35,13 +74,15 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_imgs,bb_accs=init_bb_imgs()#爆弾の大きさ、加速度
     bb_img = pg.Surface((20, 20))  # 空のSurfaceを作る（爆弾用）
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤い円を描く
     bb_img.set_colorkey((0, 0, 0))  # 黒を透明色に設定
-    bb_rct = bb_img.get_rect()  # 爆弾Rectを取得
+    bb_rct = bb_imgs[0].get_rect()  # 爆弾Rectを取得
     bb_rct.centerx = random.randint(0, WIDTH)  # 横座標用の乱数
     bb_rct.centery = random.randint(0, HEIGHT)  # 縦座標用の乱数
     vx, vy = +5, +5  # 爆弾の移動速度
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -49,7 +90,7 @@ def main():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bb_rct):  # こうかとんRectと爆弾Rectの衝突判定
-            print("ゲームオーバー")
+            gameover(screen)
             return
         screen.blit(bg_img, [0, 0]) 
 
@@ -59,14 +100,7 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
-        # if key_lst[pg.K_UP]:
-        #     sum_mv[1] -= 5
-        # if key_lst[pg.K_DOWN]:
-        #     sum_mv[1] += 5
-        # if key_lst[pg.K_LEFT]:
-        #     sum_mv[0] -= 5
-        # if key_lst[pg.K_RIGHT]:
-        #     sum_mv[0] += 5
+
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 移動をなかったことにする
